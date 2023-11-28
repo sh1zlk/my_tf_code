96e46d6cefdbfef2340907e8557675c49e5ca8ce
sh1zlk <andriykolomiec218@gmail.com>
2023-11-28 02:07:21 +0200

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,3 +11,15 @@ provider "azurerm" {
   skip_provider_registration = true 
   features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name = var.rg_name
+  location = var.rg_location
+}
+
+module "cluster" {
+  source = "./cluster"
+  rg_name = var.rg_name
+  rg_location = var.rg_location
+  depends_on = [ azurerm_resource_group.rg ]
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+  description = "Name of resource group"
+  type = string
+  default = "front-shop"
+}
+
+variable "rg_location" {
+  description = "Resource group location"
+  type = string
+  default = "Weast Europe"
+}
