053cb7505d1929e5133e4e29498d1150f1af68b3
Sh1zik <andriykolomiec218@gmail.com>
2023-11-29 11:28:17 +0200

    update

---
+++ cluster/main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name = var.cluster_name
+  resource_group_name = var.rg_name
+  location = var.rg_location
+  dns_prefix = "aks1"
+  
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.default_node_count
+    vm_size    = "Standard_D2_v2"
+    vnet_subnet_id = var.subnet_id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "nodepool_stop" {
+  enable_auto_scaling = true
+  name = "spot"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
+  node_count = var.spot_node_count
+  max_count = var.spot_node_count
+  min_count = 1
+  vm_size = "Standard_DS2_v2"
+  priority = "Spot"
+  os_type = "Linux"
+  node_labels = {
+    "kubernetes.azure.com/scalesetpriority" = "spot"
+  }
+
+  node_taints = [
+    "kubernetes.azure.com/scalesetpriority=spot:NoSchedule"
+  ]
+
+}
